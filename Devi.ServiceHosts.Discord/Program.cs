ace61bbf30f0d1cd596b586d25bb7e21af7c1e18
Sebastian Thönissen <26723285+thoenissen@users.noreply.github.com>
2023-05-10 20:17:48 +0200

    Initial solution structure

---
+++ Devi.ServiceHosts.Discord/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Devi.ServiceHosts.Discord
+{
+    /// <summary>
+    /// Program
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Main entry point
+        /// </summary>
+        /// <param name="args">Command line arguments</param>
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ scripts/SetDockerEnvironmentVariables.ps1
@@ -0,0 +1,6 @@
+﻿foreach($line in Get-Content .\Devi.ServiceHosts.Discord\Docker.env) 
+{
+	$tmp = $line.Split("=")
+
+	[System.Environment]::SetEnvironmentVariable($tmp[0], $tmp[1])
+}
