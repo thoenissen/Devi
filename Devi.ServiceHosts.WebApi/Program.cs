b28703e08fc3fb30ae853ec94d5f0999f27b537d
Sebastian Thönissen <26723285+thoenissen@users.noreply.github.com>
2023-05-14 15:29:51 +0200

    Added OpenSearch sink

--- Devi.ServiceHosts.WebApi/Program.cs
+++ Devi.ServiceHosts.WebApi/Program.cs
@@ -13,6 +13,11 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
+using OpenSearch.Net;
+
+using Serilog;
+using Serilog.Sinks.OpenSearch;
+
 namespace Devi.ServiceHosts.WebApi;
 
 /// <summary>
@@ -27,7 +32,46 @@ public class Program
     /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
     public static async Task Main(string[] args)
     {
+        var loggerConfiguration = new LoggerConfiguration().Enrich.FromLogContext()
+                                                           .WriteTo.Console(outputTemplate: "[{Timestamp:yyyy-MM-dd HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}");
+
+        var openSearchUrl = Environment.GetEnvironmentVariable("DEVI_OPENSEARCH_URL");
+        var environment = Environment.GetEnvironmentVariable("DEVI_ENVIRONMENT");
+
+        if (string.IsNullOrEmpty(openSearchUrl) == false
+         && string.IsNullOrEmpty(environment) == false)
+        {
+            Func<ConnectionConfiguration, ConnectionConfiguration> modifyConnectionSettings = null;
+
+            var user = Environment.GetEnvironmentVariable("DEVI_OPENSEARCH_USER");
+
+            if (string.IsNullOrWhiteSpace(user) == false)
+            {
+                modifyConnectionSettings = obj =>
+                                           {
+                                               obj.BasicAuthentication(user, Environment.GetEnvironmentVariable("DEVI_OPENSEARCH_PASSWORD"));
+
+                                               // HACK / TODO - Create real certificate
+                                               obj.ServerCertificateValidationCallback(CertificateValidations.AllowAll);
+                                               return obj;
+                                           };
+            }
+
+            loggerConfiguration.WriteTo.OpenSearch(new OpenSearchSinkOptions(new Uri(Environment.GetEnvironmentVariable("DEVI_OPENSEARCH_URL")))
+                                                   {
+                                                       AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.OSv2,
+                                                       AutoRegisterTemplate = true,
+                                                       MinimumLogEventLevel = Serilog.Events.LogEventLevel.Verbose,
+                                                       IndexFormat = $"devi-{environment}-{DateTime.Now:yyyy-MM}",
+                                                       ModifyConnectionSettings = modifyConnectionSettings
+                                                   });
+        }
+
+        Log.Logger = loggerConfiguration.CreateBootstrapLogger();
+
         var builder = WebApplication.CreateBuilder(args);
+
+        builder.Host.UseSerilog();
 
         builder.Services.AddControllers();
         builder.Services.AddEndpointsApiExplorer();
